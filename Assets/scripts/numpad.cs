55b724102ae9c097c8681349910183fb3a4f9eca
Yiannir <yiannis.rav@gmail.com>
2020-02-01 14:34:25 +0000

    Added radar.

---
+++ Assets/scripts/numpad.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro; // Add the TextMesh Pro namespace to access the various functions.
+
+public class numpad : MonoBehaviour
+{
+    public bool isrunning = true;
+
+    string to_enter;
+    public string entered;
+
+    float nextbreak = 0;
+    public float now;
+    public float breaktime;
+    public float mintime = 2;
+    public float maxtime = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextbreak = Time.time + Random.Range(mintime, maxtime);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Find("screen/text").GetComponent<TextMeshPro>().SetText(entered);
+
+        now = Time.time;
+        breaktime = nextbreak;
+
+        if (isrunning)
+        {
+            //Light light = this.transform.Find("screen/light").GetComponent<Light>();
+            //light.enabled = false;
+
+            if (Time.time > nextbreak)
+            {
+                isrunning = false;
+            }
+        }
+        else //not running
+        {
+            //Light light = this.transform.Find("screen/light").GetComponent<Light>();
+            //light.enabled = true;
+          
+            if (this.transform.Find("0").GetComponent<small_button>().pressed) { entered += "0"; }
+            if (this.transform.Find("1").GetComponent<small_button>().pressed) { entered += "1"; }
+            if (this.transform.Find("2").GetComponent<small_button>().pressed) { entered += "2"; }
+            if (this.transform.Find("3").GetComponent<small_button>().pressed) { entered += "3"; }
+            if (this.transform.Find("4").GetComponent<small_button>().pressed) { entered += "4"; }
+            if (this.transform.Find("5").GetComponent<small_button>().pressed) { entered += "5"; }
+            if (this.transform.Find("6").GetComponent<small_button>().pressed) { entered += "6"; }
+            if (this.transform.Find("7").GetComponent<small_button>().pressed) { entered += "7"; }
+            if (this.transform.Find("8").GetComponent<small_button>().pressed) { entered += "8"; }
+            if (this.transform.Find("0").GetComponent<small_button>().pressed) { entered += "9"; }
+
+            if(entered.Length > 4)
+            {
+                entered = entered.Substring(1, 4);
+            }
+
+            if(entered == to_enter)
+            {
+                isrunning = true;
+                nextbreak = Time.time + Random.Range(mintime, maxtime);
+            }
+
+        }
+    }
+
+    string randomint()
+    {
+        int PIN = Random.Range(0, 9);
+        return PIN.ToString();
+    }
+}
+
+
---
+++ Assets/scripts/radarscreen.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class radarscreen : MonoBehaviour
+{
+    Vector3 movement;                   // The vector to store the direction of the player's movement.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float z = Input.GetAxisRaw("Horizontal");
+        float x = Input.GetAxisRaw("Vertical");
+
+        // Set the movement vector based on the axis input.
+        movement.Set(x/10, 0f, z/10);
+        // Move the player to it's current position plus the movement.
+        this.transform.Find("light").transform.Translate(movement);
+    }
+}
