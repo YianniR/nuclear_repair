7b93f2f07214083c165ae2b37d7cd64ea91c769d
Yiannir <yiannis.rav@gmail.com>
2020-02-01 18:01:14 +0000

    working radar

--- Assets/scripts/radarscreen.cs
+++ Assets/scripts/radarscreen.cs
@@ -4,28 +4,41 @@ using UnityEngine;
 
 public class radarscreen : MonoBehaviour
 {
-    Vector3 force;                   // The vector to store the direction of the player's movement.
-    Vector3 randomForce;                   // The vector to store the direction of the player's movement.
-    public Rigidbody _light;
+    Vector3 velocity;                   // The vector to store the direction of the player's movement.
+    Vector3 randomvelocity;                   // The vector to store the direction of the player's movement.
+    public GameObject _light;
+    float nextbreak = 0;
+    public float maxRadius;
 
     // Start is called before the first frame update
     void Start()
     {
-        _light = transform.Find("radarlight").gameObject.GetComponent<Rigidbody>();
+        _light = transform.Find("radarlight").gameObject;
+        nextbreak = Time.time + Random.Range(5, 10);
+
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         float z = Input.GetAxisRaw("Horizontal");
         float x = Input.GetAxisRaw("Vertical");
 
-        randomForce.Set(Random.Range(-0.2f, 0.2f), 0f, Random.Range(-0.2f, 0.2f));
-        force.Set(-x, 0f, z);
-        _light.AddForce(force);
-        _light.AddForce(randomForce);
+        velocity.Set(x/10, 0f, z/10);
+        _light.transform.localPosition += velocity;
 
-        //_light.position = Vector3.ClampMagnitude(_light.position, 2.5f);
+        if (Time.time > nextbreak)
+        {
 
+            randomvelocity.Set(Random.Range(-0.01f, 0.01f), 0f, Random.Range(-0.01f, 0.01f));
+
+            nextbreak = Time.time + Random.Range(5, 10);
+        }
+
+        _light.transform.localPosition += randomvelocity;
+
+        Vector3 pos = new Vector3(_light.transform.localPosition.x, 0f, _light.transform.localPosition.z);
+        pos = Vector3.ClampMagnitude(pos, maxRadius);
+        _light.transform.localPosition = new Vector3(pos.x, _light.transform.localPosition.y, pos.z);
     }
 }
