3804af920a202710e7b69589f36fe4f413cfc3d1
Yiannir <yiannis.rav@gmail.com>
2020-02-01 16:36:16 +0000

    Shoot first ask questions later.

--- Assets/scripts/radarscreen.cs
+++ Assets/scripts/radarscreen.cs
@@ -10,7 +10,7 @@ public class radarscreen : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        _light = GameObject.Find("light").GetComponent<Rigidbody>();
+        _light = transform.Find("radarlight").gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
