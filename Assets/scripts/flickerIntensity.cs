1ec70db70e886e0984991ee01f88c1d9700892b1
Yiannir <yiannis.rav@gmail.com>
2020-02-01 20:09:00 +0000

    LOLS WHOO CARES

---
+++ Assets/scripts/flickerIntensity.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flickerIntensity : MonoBehaviour
+{
+    float change;
+    public float minChange = -0.2f;
+    public float maxChange = 0.2f;
+
+    public float minInt = 0.85f;
+    public float maxInt = 1.25f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        change = Random.Range(minChange, maxChange);
+        this.GetComponent<Light>().intensity += change;
+        this.GetComponent<Light>().intensity = Mathf.Clamp(this.GetComponent<Light>().intensity, minInt, maxInt);
+    }
+}
