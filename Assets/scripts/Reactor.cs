a102c32cc4aedaf5cc6b518ea230ace5dc3c007d
Yiannir <yiannis.rav@gmail.com>
2020-02-01 20:46:39 +0000

    Update Reactor script.

--- Assets/scripts/Reactor.cs
+++ Assets/scripts/Reactor.cs
@@ -3,27 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Reactor : MonoBehaviour {
-    public int id;
-    public int temperature;
-    public float secondsPerTemperatureIncrease = 1;
-
+    public float health = 10000f;
 
     private float untilNextTemperatureIncrease;
 
     // Start is called before the first frame update
     void Start() {
-        untilNextTemperatureIncrease = secondsPerTemperatureIncrease;
+
     }
 
     // Update is called once per frame
     void Update() {
-        Debug.Log(temperature);
+        Debug.Log(health);
+       
 
-        untilNextTemperatureIncrease -= Time.deltaTime;
-
-        if (untilNextTemperatureIncrease <= 0) {
-            temperature += 1;
-            untilNextTemperatureIncrease = secondsPerTemperatureIncrease;
-        }
+        float scaledlevel = 0.1f + 0.9f * health;
+        this.transform.Find("health/bar").localScale = new Vector3(1, scaledlevel, 1);
     }
 }
