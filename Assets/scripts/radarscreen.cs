5e32f6ee00095d54d53e8fad886dd86952303896
Yiannir <yiannis.rav@gmail.com>
2020-02-01 16:54:44 +0000

    SCRIP

--- Assets/scripts/radarscreen.cs
+++ Assets/scripts/radarscreen.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class radarscreen : MonoBehaviour
 {
     Vector3 force;                   // The vector to store the direction of the player's movement.
+    Vector3 randomForce;                   // The vector to store the direction of the player's movement.
     public Rigidbody _light;
 
     // Start is called before the first frame update
@@ -19,8 +20,12 @@ public class radarscreen : MonoBehaviour
         float z = Input.GetAxisRaw("Horizontal");
         float x = Input.GetAxisRaw("Vertical");
 
+        randomForce.Set(Random.Range(-0.2f, 0.2f), 0f, Random.Range(-0.2f, 0.2f));
         force.Set(-x, 0f, z);
         _light.AddForce(force);
+        _light.AddForce(randomForce);
+
+        //_light.position = Vector3.ClampMagnitude(_light.position, 2.5f);
 
     }
 }
